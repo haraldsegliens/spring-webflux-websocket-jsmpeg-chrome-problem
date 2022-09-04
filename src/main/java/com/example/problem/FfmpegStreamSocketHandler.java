2abdf58294e2a848fc26d026cabacf27d7b21003
Haralds Egliens <haralds.egliens@tieto.com>
2022-09-04 12:17:22 +0300

    solution: use-protocol

--- src/main/java/com/example/problem/FfmpegStreamSocketHandler.java
+++ src/main/java/com/example/problem/FfmpegStreamSocketHandler.java
@@ -6,6 +6,8 @@ import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.WebSocketSession;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
 @Component
 public class FfmpegStreamSocketHandler implements WebSocketHandler {
     private final FfmpegStreamRunner ffmpegStreamRunner;
@@ -17,5 +19,10 @@ public class FfmpegStreamSocketHandler implements WebSocketHandler {
     @Override
     public Mono<Void> handle(WebSocketSession session) {
         return session.send(ffmpegStreamRunner.getFfmpegStream().map(dataBuffer -> new WebSocketMessage(WebSocketMessage.Type.BINARY, dataBuffer)));
+    }
+
+    @Override
+    public List<String> getSubProtocols() {
+        return List.of("mpeg-ts");
     }
 }
